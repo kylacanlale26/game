a1ea85411afd2a13a0b57517e191f964656d9c5a
Kyla Lacanlale <ky.lacanlale.26@gmail.com>
2026-05-04 20:02:23 +0800

    Initial commit

---
+++ game.py
@@ -0,0 +1,10 @@
+# Simple dungeon text based game
+# where the player can input their name
+# have their own stats
+# you provide the player classes to choose from your own declared classes
+# attack system
+# defense and skill mechanism or additional mechanisim
+# complete
+# gold and shop system
+# end game
+# use list, tuple, dictionary, set
