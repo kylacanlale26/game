e489cc5cebdb4052a3f06f5904f2f772a74c444c
Kyla Lacanlale <ky.lacanlale.26@gmail.com>
2026-05-05 00:57:50 +0800

    new files created

---
+++ enemy.py
@@ -0,0 +1,8 @@
+#enemies stats - list
+monsters = [
+    ("Minion", 10, 100),
+    ("Dark Husk", 20, 200),
+    ("Witch", 15, 120),
+    (""),
+    ("Dragon", 50, 500)
+]
---
+++ player_data.py
@@ -0,0 +1,42 @@
+import random
+
+#player data - name, class and stats, and additional data
+def player_data():
+
+    #input - player's name
+    player_name = input("Enter Name: ")
+
+    #dictionary - classes along with their respective stats that the players can choose from
+    class_choices = {
+        "Knight": {"hp": 120, "attack": 30, "defense": 25, "skill": "Power Slash"},
+        "Assassin": {"hp": 100, "attack": 50, "defense": 15, "skill": "Shadow Strike"},
+        "Mage": {"hp": 80, "attack": 40, "defense": 5, "skill": "Wind Punch"},
+        "Healer": {"hp": 300, "attack": 10, "defense": 20, "skill": "Divine Healing"},
+        "Tank": {"hp": 150, "attack": 25, "defense": 200, "skill": "Iron Clad"}
+    }
+
+    #player choice input
+    print("\nClasses: ")
+    for c in class_choices:
+        print("-", c)
+
+    choice = input("\nChoose your Class: ").title()
+
+    #validate choice
+    while choice not in class_choices:
+        choice = input("\nSelected class invalidKyla. Choose again: ").title()
+
+    #copy selected class' stats
+    player = class_choices[choice].copy()
+
+    #additional player data - add items to dictionary class_choice
+    player["name"] = player_name
+    player["class"] = choice
+    player["gold"] = 150
+    player["inventory"] = []
+    player["skills_used"] = set()
+
+    return player
+
+player = player_data()
+print(player)
