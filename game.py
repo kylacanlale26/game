acf248ad0e701aee55181cf8728508c649bceb1d
Kyla Lacanlale <ky.lacanlale.26@gmail.com>
2026-05-04 23:38:43 +0800

    update - player class choice input & class + stats and other player data display

--- game.py
+++ game.py
@@ -8,18 +8,49 @@
 # gold and shop system
 # end game
 # use list, tuple, dictionary, set
+ 
+#===START OF CODE===#
 
 import random
 
-#user input details
-def player_details():
+#player data
+def player_data():
+
+    #input - player's name
     player_name = input("Enter Name: ")
 
     #dictionary - classes along with their respective stats that the players can choose from
-    player_class = {
-        "Knight": {"HP": 150, "Attack": 30, "Defense": 25, "Skill": "Power Slash"},
-        "Assasin": {"HP": 100, "Attack": 50, "Defense": 15, "Skill": "Shadow Strike"},
-        "Mage": {"HP": 80, "Attack": 40, "Defense": 5, "Skill": "Wind Punch"},
-        "Healer": {"HP": 300, "Attack": 10, "Defense": 20, "Skill": "Divine Healing"},
-        "Tank": {"HP": 200, "Attack": 25, "Defense": 200, "Skill": "Iron Skin"}
+    class_choices = {
+        "Knight": {"hp": 120, "attack": 30, "defense": 25, "skill": "Power Slash"},
+        "Assassin": {"hp": 100, "attack": 50, "defense": 15, "skill": "Shadow Strike"},
+        "Mage": {"hp": 80, "attack": 40, "defense": 5, "skill": "Wind Punch"},
+        "Healer": {"hp": 300, "attack": 10, "defense": 20, "skill": "Divine Healing"},
+        "Tank": {"hp": 150, "attack": 25, "defense": 200, "skill": "Iron Clad"}
     }
+
+    #player choice input
+    print("\nClasses: ")
+    for c in class_choices:
+        print("-", c)
+
+    choice = input("\nChoose your Class: ").title()
+
+    #validate choice
+    while choice not in class_choices:
+        choice = input("\nSelected class invalidKyla. Choose again: ").title()
+
+    #copy selected class stats
+    player = class_choices[choice].copy()
+
+    #additional player data
+    player["name"] = player_name
+    player["class"] = choice
+    player["gold"] = 150
+    player["inventory"] = []
+    player["skills_used"] = set()
+
+    return player
+
+player = player_data()
+
+print(player)
