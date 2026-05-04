3e4a19772987dbbd006de2212f8be8e02ef8d026
Kyla Lacanlale <ky.lacanlale.26@gmail.com>
2026-05-04 23:04:33 +0800

    update - player name input & player class dictionary

--- game.py
+++ game.py
@@ -8,3 +8,18 @@
 # gold and shop system
 # end game
 # use list, tuple, dictionary, set
+
+import random
+
+#user input details
+def player_details():
+    player_name = input("Enter Name: ")
+
+    #dictionary - classes along with their respective stats that the players can choose from
+    player_class = {
+        "Knight": {"HP": 150, "Attack": 30, "Defense": 25, "Skill": "Power Slash"},
+        "Assasin": {"HP": 100, "Attack": 50, "Defense": 15, "Skill": "Shadow Strike"},
+        "Mage": {"HP": 80, "Attack": 40, "Defense": 5, "Skill": "Wind Punch"},
+        "Healer": {"HP": 300, "Attack": 10, "Defense": 20, "Skill": "Divine Healing"},
+        "Tank": {"HP": 200, "Attack": 25, "Defense": 200, "Skill": "Iron Skin"}
+    }
